5c0030fc3411d33a98c96a25897e185a9f62d37b
Rich Harkins <rich.harkins@gmail.com>
2022-07-05 04:59:23 +0000

    Rewrite for better parsing.
    Need to restore formatting.

--- gql.bash
+++ gql.bash
@@ -1,408 +1,320 @@
 #!/usr/bin/env bash
 
 ##############################################################################
-
-declare -Ar _GQL_OP_MAPPINGS=(
-    [get]='query'
-    [q]='query'
-    [m]='mutation'
-    [mut]='mutation'
-    [do]='mutation'
-    [s]='subscription'
-    [sub]='subscription'
-    [wait]='subscription'
-    [iter]='subscription'
-)
+##############################################################################
 
 function gql
 {
-    gql.setup "${GQL_PROFILE:-default}"
-    local OPTARG OPTIND OPT
-    local GQL_URL="${GQL_URL}"
-    local SHOW_QUERY SHOW_JQ SHOW_STRUCT SHOW_OUTPUT DEBUG_FORMATTER RAW_JSON
-    local SHOW_HEADERS
+    local OPTIND OPTARG OPT
+    local op=query 
+    local GQL_URL="${GQL_URL}" 
+    local GQL_PROFILE="${GQL_PROFILE}"
+    local GQL_LOG_LEVEL="${GQL_LOG_LEVEL:-0}"
+    local dry_run
     
-    while getopts 'u:QJFSORPHN' OPT
+    while getopts 'du:p:qmsO:n' OPT
     do
         case "$OPT" in
+        d) GQL_LOG_LEVEL=-2;;
+        q) op=query;;
+        m) op=mutatiion;;
+        s) op=subscription;;
+        O) op="$OPTARG";;
         u) GQL_URL="$OPTARG";;
-        Q) SHOW_QUERY=1;;
-        J) SHOW_JQ=1;;
-        F) DEBUG_FORMATTER=1;;
-        H) SHOW_HEADERS=1;;
-        S) SHOW_STRUCT=1;;
-        O) SHOW_OUTPUT=1;;
-        R) RAW_JSON=1;;
-        P) RAW_JSON=pretty;;
-        N) SHOW_OUTPUT=none;;
+        p) GQL_PROFILE="$OPTARG";;
+        n) dry_run=1;;
         esac
     done
-    
     shift $(( OPTIND - 1 ))
     
-    local op="$1"; shift
-    local mapped="${_GQL_OP_MAPPINGS[${op,}]:-${op,,}}"
-    case "${mapped}" in
+    local -A _gql_config
+    _gql_copy_dict "${GQL_PROFILE:-GQL_CONFIG}" _gql_config
     
-    query)
-        local -A parsed
-        local gql
-        gql.build parsed "$@" 
-        gql.dump parsed query gql
-        gql.exec "$gql" | gql.output parsed
-        ;;
+    if [ "$GQL_URL" ]
+    then
+        _gql_config[url]="$GQL_URL"
+    fi
     
-    '')
-        {
-            echo "USAGE: gql [op]"
-        } >&2
-        exit 65;;
+    case "$op" in
+        query|mutation|subscription)
         
-    *)
-        echo >&2 "Invalid op '$op'"
-        exit 1;;
-
+            local -A _gql_doc
+            gql.build _gql_doc "$op" data "$@"
+            gql.format _gql_doc query |
+            tee /dev/stderr |
+            if [ "$dry_run" ]
+            then
+                cat -
+            else
+                gql.agent "$GQL_URL" 
+            fi
+            ;;
+            
+        *)
+            _gql_usage_error "Op '$op' is not valid"
+            ;;
     esac
 }
 
-function gql.setup
-{
-    declare -gn "_GQLCFG_${GQL_PROFILE:-_$1}"
-}
-
+##############################################################################
 ##############################################################################
 
 function gql.build
 {
-    local -n _dest="$1"; shift || gql.required dest
+    local -n _doc="$1"; shift || _gql_required 'gql document variable reference'
+    local docpath="$1"; shift || _gql_required 'gql document path'
+    local datapath="$1"; shift || _gql_required 'gql result data path'
+    
     local -a _args=( "$@" )
-    local -i _idx=0 _nargs=${#_args[@]}
-    local prefix _strip_prefix
-    local -n _outputs="_dest[$prefix:outputs]"
-    local -n _tables="_dest[$prefix:tables]"
-    _gql_build '' ''
-    if [ "$SHOW_STRUCT" ]
-    then
-        local field
-        for field in "${!_dest[@]}"
-        do
-            echo -e "$field\t${_dest["$field"]}"
-        done | sort
-    fi
+    local -i _idx=0 _nargs="${#_args[@]}"
+    
+    _gql_build "$docpath" "$datapath"
 }
 
 function _gql_build
 {
-    local node="$1"; shift || gql.required node
-    local prefix="$1"; shift || gql.required prefix
-    local -n _params="_dest[$prefix:args]" _paramstr="_dest[$prefix:argstr]"
-    local -n _fields="_dest[$prefix:fields]" _fieldstr="_dest[$prefix:fieldstr]"
-  
-    _gql_build_params "$@"
-}
+    local docpath="$1"; shift || _gql_required 'gql document path'
+    local datapath="$1"; shift || _gql_required 'gql result data path'
+    local buildpath="${1:-$docpath}"
+    
+    # These two references will be based on the value of $buildpath upon use.
+    local -n paramstr='_doc[$buildpath:paramstr]'
+    local -n fields='_doc[$buildpath:fields]'
+    
+    _doc["${docpath}:node"]="${docpath##*.}"
 
-function _gql_build_params
-{ 
     while [ $_idx -lt $_nargs ]
     do
         local arg="${_args[$_idx]}"
         case "$arg" in
-        '[')
-            local -n _outputs="_dest[$prefix:outputs]"
-            _tables+="${_tables:+ }$prefix"
-            _dest["$prefix:format"]='table'
-            ;&
-
-        '{{')
-            local _strip_prefix="$prefix."
-            ;&
-        
         '{')
             _idx+=1
-            _gql_build_fields "$@"
-            return
+            _gql_build "$buildpath" "$buildpath"
             ;;
             
-        ']' | '}' | '}}')
-            return
-            ;;
-        
-        *)
-            local name value="${arg#*=}"
-            if [ "$value" != "$arg" ]
-            then
-                name="${arg%%=*}"
-                value="${value/\"//\\\"}"
-                value="${value/\n//\\n}"
-                value="\"$value\""
-            else
-                value="${arg#*:}"
-                if [ "$value" != "$arg" ]
-                then
-                    name="${arg%%:*}"
-                fi
-            fi
-            
-            if [ "$name" ]
-            then
-                if [ "${name#-}" != "$name" ]
-                then
-                    _dest["$prefix:$name"]="$value"
-                elif [ "$value" ]
-                then
-                    if [ $# = 0 ]
-                    then
-                        name="\$${name#\$}"
-                    fi
-                    _params+="${_params:+ }${name}"
-                    _paramstr+="${_paramstr:+, }${name}: ${value}"
-                else
-                    # Alias decalration, handled in fields...
-                    return
-                fi                    
-            else
-                #_gql_build_fields
-                return
-            fi
+        '}')
             _idx+=1
+            return
+            ;;
+
+        *)
+            {
+                local node suffix name value="${arg#*=}"
+                _idx+=1
+                
+                if [ "$arg" != "$value" ]
+                then
+                    name="${arg%%=*}"
+                    value="${value//\"/\\\"}"
+                    value="${value//\n/\\n}"
+                    value="\"$value\""
+                else                    
+                    value="${arg#*:}"
+                    if [ "$arg" != "$value" ]
+                    then
+                        name="${arg%%:*}"
+                        _gql_dump name value
+                    fi
+                fi
+                
+                if [ "$name" ]
+                then
+                    if [ "$value" ]
+                    then
+                        paramstr+="${paramstr:+, }$name: $value"
+                    else
+                        : alias...
+                    fi
+                else
+                    buildpath="$docpath"
+                    while true
+                    do
+                        suffix="${arg#*.}"
+                        if [ "$suffix" = "$arg" ]
+                        then
+                            break
+                        else
+                            {
+                                node="${arg%%.*}"
+                                if [ ! "${_doc["$buildpath.$node:fields"]}" ]
+                                then
+                                    fields+="${fields:+ }$node"
+                                fi                                    
+                                buildpath+=".$node"
+                                _doc["$buildpath:node"]="$node"
+                                arg="$suffix"
+                            }
+                        fi
+                    done
+                    fields+="${fields:+ }$arg"
+                    _doc["$buildpath.$arg:node"]="$arg"
+                    buildpath="$buildpath.$arg"
+                fi
+            }                
             ;;
         esac
-        
-    done
-}
-
-function _gql_build_fields
-{
-    local alias
-    while [ $_idx -lt $_nargs ]
-    do
-        local arg="${_args[$_idx]}"
-        case "$arg" in
-        '{{' | '{' | '[')
-            # This is an error...
-            _idx+=1
-            return
-            ;;
-            
-        '}' | ']' | '}}')
-            _idx+=1
-            break
-            ;;
-        
-        *)
-            if [ "${arg%:}" = "$arg" ]
-            then
-                _idx+=1
-                local child="${alias:-$arg}"
-                local stripped="${prefix#$_strip_prefix}"
-                if [ "$stipped" != "$prefix" ]
-                then
-                    _dest["$prefix.$child:label"]="$stripped${stripped:+.}$child"
-                fi
-                _fields+="${_fields:+ }${child}"
-                _gql_build "$child" "$prefix.$child" "$child" "$@"
-                if [ "$alias" ]
-                then
-                    _dest["$prefix.$child:alias-for"]="$arg"
-                    alias=
-                fi
-                if [ ! "${_dest["$prefix.$child":fields]}" ]
-                then
-                    _outputs+="${_outputs:+ }$prefix.$child"
-                fi
-            else
-                _idx+=1
-                alias="${arg%:}"
-            fi
-            ;;
-        esac            
     done
 }
 
 ##############################################################################
+##############################################################################
 
-function gql.exec
+function gql.format
 {
-    local gql="$1"; shift || gql.required 'graphql query'
-    
-    if [ "$SHOW_QUERY" ]
-    then
-        echo >&2 "$gql"
-    fi
-    
-    "${GQL_AGENT:-gql.curl}" "$gql" "$GQL_URL"
+    local -n _doc="$1"; shift || _gql_required 'gql document variable reference'
+    local docpath="$1"; shift || _gql_required 'gql document path'
+    local prefix="$1"; shift
+    local indent="${1:-    }"; shift
+
+    _gql_format "$docpath" "$prefix"
 }
 
-function gql.curl
+function _gql_format
 {
-    local gql="$1"; shift || gql.required 'graphql query'
-    local url="$1"; shift || gql.required 'graphql url'
-    echo '{}' | jq \
-        --arg query "$gql" \
-    '{
-        "query": $query
+    local docpath="$1"; shift || _gql_required 'gql document path'
+    local prefix="$1"; shift
+    
+    echo -n "$prefix${_doc["$docpath":node]}"
+    
+    local paramstr="${_doc["$docpath":paramstr]}"
+    if [ "$paramstr" ]
+    then
+        echo -n "($paramstr)"
+    fi
+    
+    local field fields="${_doc["$docpath":fields]}"
+    if [ "$fields" ]
+    then
+        echo " {"
+        for field in ${_doc["$docpath":fields]}
+        do
+            _gql_format "$docpath.$field" "$prefix$indent"
+        done
+        echo "$prefix}"
+    else
+        echo
+    fi
+}
+
+##############################################################################
+##############################################################################
+
+function gql.agent
+{
+    "${GQL_AGENT:-gql.agent.curl}" "$@"
+}
+
+function gql.agent.curl
+{
+    local OPTIND OPTARG OPT
+    local url="${GQL_URL}"
+    local gql='-'
+    
+    while getopts 'u:' OPT
+    do
+        case "$OPT" in
+        u) url="$OPTARG";;
+        d) gql="$OPTARG";;
+        esac
+    done
+    shift $(( OPTIND - 1 ))
+    
+    if [ "$gql" = - ]
+    then
+        gql="$(cat -)"
+    fi
+    
+    echo {} | jq --arg gql "$gql" '
+    {
+        "query": $gql
     }' |
-    # tee /dev/stderr |
-    curl -s -X POST --data-binary @- \
+    curl \
+        -X POST \
         -H 'Content-Type: application/json' \
+        -H 'Accept: application/json' \
+        --data-binary @- \
         "$url"
 }
 
 ##############################################################################
-
-function gql.dump
-{
-    local _src="$1"; shift || gql.required source
-    local op="$1"; shift || gql.required operation
-    local dest="$1"; shift
-    local -a terms
-    
-    gql.format "$_src" terms "$op"
-    if [ "$dest" ]
-    then
-        local -n _dest="$dest"
-        _dest="${terms[@]}"
-    else
-        echo "${terms[@]}"
-    fi        
-}
-
-function gql.format
-{
-    local -n _src="$1"; shift || gql.required source
-    local -n _dest="$1"; shift || gql.required destination
-    local node="$1"; shift || gql-required node
-    local scope="$1"; shift
-
-    local alias_for="${_src["$scope:alias-for"]}"
-    if [ "$alias_for" ]
-    then
-        _dest+=( "$node: $alias_for" )
-    else
-        _dest+=( "$node" )
-    fi
-    
-    local argstr="${_src["$scope:argstr"]}"
-    if [ "$argstr" ]
-    then
-        _dest+=( '(' "$argstr" ')' )
-    fi
-    
-    local fields="${_src["$scope:fields"]}"
-    if [ "$fields" ]
-    then
-        _dest+=( '{' )
-        
-        local field
-        for field in $fields
-        do
-            gql.format "${!_src}" "${!_dest}" "$field" "$scope.$field"
-        done
-        
-        _dest+=( '}' )
-    fi
-}
-
 ##############################################################################
 
-function gql.output
+function _gql_log
 {
-    if [ "$SHOW_OUTPUT" -a "$SHOW_OUTPUT" != none ]
+    local -i level="$1"; shift || _gql_required 'log level'
+    if [ $level -ge ${GQL_LOG_LEVEL:-0} ]
     then
-        tee /dev/stderr
-    else
-        cat -
-    fi |
-    
-    "gql.format.${GQL_FORMATTER:-jq}" "$@" |
-    if [ "$SHOW_OUTPUT" != none ]
-    then
-        cat -
-    else
-        cat >/dev/null
-    fi
-}
-
-function gql.format.jq
-{
-    local jq
-    local -n config="$1"; shift || gql.required config 
-    
-    local outputs="${config[':outputs']}"
-    if [ "$outputs" ]
-    then
-        local output
-        for output in $outputs
-        do
-            local label="${config["$output:label"]:-$output}"
-            jq+="${jq:+,}([\"$label\",.data$output]|@tsv)"
-        done
-    fi        
-        
-    local table_prefix
-    for table_prefix in ${config[':tables']}
-    do
-        local -a headers
-        local table_field table_jq
-        for table_field in ${config["$table_prefix:outputs"]}
-        do
-            table_field="${table_field#$table_prefix}"
-            table_jq+="${table_jq:+,}$table_field"
-            
-            headers+=( "${config["$table_field:label"]:-$table_field}" )
-        done
-        local header_jq header
-        if [ "$SHOW_HEADERS" ]
+        if [ $# -gt 0 ]
         then
-            for header in "${headers[@]}"
-            do
-                header_jq+="${header_jq:+,}\"$header\""
-            done
-            header_jq="[$header_jq],"
-        fi            
-        jq+="${jq:+,}(($header_jq(.data$table_prefix[]|[$table_jq]))|@tsv)"
-    done
-    
-    if [ "$DEBUG_FORMATTER" -o "$SHOW_JQ" ]
+            echo "$@"
+        else
+            cat -
+        fi >&2
+    elif [ $# = 0 ]
     then
-        echo "JQ: $jq" >&2
+        cat - >/dev/null
     fi
-    
-    if [ "$RAW_JSON" = '' -a "$jq" ]
-    then
-        jq -r "
-            if has(\"errors\")
-            then
-                .errors[] |
-                (
-                    error(.message)
-                )
-            else
-                ($jq)
-            end                
-        "
-    elif [ "$RAW_JSON"=pretty -o -t 1 ]
-    then
-        jq .
-    else
-        cat -
-    fi            
 }
 
 ##############################################################################
 
-function gql.required
+function _gql_debug         { _gql_log -2 "$@" at "$(caller 0)"; }
+function _gql_debug_raw     { _gql_log -2 "$@"; }
+function _gql_verbose       { _gql_log -1 "$@"; }
+function _gql_info          { _gql_log  0 "$@"; }
+function _gql_warning       { _gql_log  1 "$@"; }
+function _gql_error         { _gql_log  2 "$@"; }
+
+function _gql_fatal
 {
-    gql.fatal required-arg missing "$@" in "$(caller 1)"
+    local -i exit_code="$1"; shift || _gql_required 'exit code'
+    _gql_log 9999 "$@"
+    exit $exit_code
 }
 
-function gql.fatal
+function _gql_dump
 {
-    local type="$1"; shift
-    echo >&2 "$@"
-    exit 1
+    _gql_debug_raw ""
+    _gql_debug_raw "At $(caller 0):" 
+    local _var
+    for _var in "$@"
+    do
+        declare -p "$_var"
+    done | _gql_debug_raw
 }
 
 ##############################################################################
 
-( return 0 2>/dev/null ) && gql_setup "$@" || gql "$@"
+function _gql_internal_error
+{
+    _gql_fatal 63 "$@"
+}
+
+function _gql_usage_error
+{
+    _fql_fatal 65 "$@"
+}    
+
+function _gql_required
+{
+    _gql_internal_error "$(caller 0) requires '$1'${@:+: }$@ from $(caller 1)"
+}
+
+##############################################################################
+##############################################################################
+
+function _gql_copy_dict
+{
+    local -n src="$1"; shift || _gql_required 'dict copy source'
+    local -n dest="$1"; shift || _gql_required 'dict copy source'
+    local key
+    for key in "${!src[@]}"
+    do
+        dest["$key"]="${src[$key]}"
+    done
+}
+
+##############################################################################
+##############################################################################
+
+( return 0 2>/dev/null ) || gql "$@"
