c4a34dab3284ff697a0c1ca094c17af9a0289414
Rich Harkins <rich.harkins@gmail.com>
2022-07-08 01:00:44 +0000

    Added basic OAUTH

--- gql.bash
+++ gql.bash
@@ -5,31 +5,34 @@
 
 function gql
 {
-    local OPTIND OPTARG OPT
-    local op=query 
-    local GQL_URL="${GQL_URL}" 
-    local GQL_PROFILE="${GQL_PROFILE}"
-    local GQL_LOG_LEVEL="${GQL_LOG_LEVEL:-0}"
-    local dry_run gql
+    #set -eu
     
-    while getopts 'dDu:p:qmsO:nG:S' OPT
+    local OPTIND OPTARG OPT
+    local op=query
+    local GQL_URL="${GQL_URL}"
+    local GQL_PROFILE="${GQL_PROFILE:-}"
+    local GQL_LOG_LEVEL="${GQL_LOG_LEVEL:-0}"
+    local GQL_SSO="${GQL_SSO}"
+    local dry_run gql
+    local GQL_SSO
+    
+    while getopts 'dDu:p:qmsO:nG:' OPT
     do
         case "$OPT" in
         d) GQL_LOG_LEVEL=-2;;
         q) op=query;;
-        m) op=mutatiion;;
+        m) op=mutation;;
         s) op=subscription;;
         O) op="$OPTARG";;
         u) GQL_URL="$OPTARG";;
         p) GQL_PROFILE="$OPTARG";;
         n) dry_run=1;;
         G) gql="$OPTARG";;
-        S) op='schema';;
         D) dry_run=doc;;
         esac
     done
     shift $(( OPTIND - 1 ))
-    
+
     local -A _gql_config
     _gql_copy_dict "${GQL_PROFILE:-GQL_CONFIG}" _gql_config
     
@@ -37,12 +40,12 @@ function gql
     then
         _gql_config[url]="$GQL_URL"
     fi
-    
+
     case "$op" in
         query|mutation|subscription)
             {
                 local -A _gql_doc
-                if [ ! "$gql" ]
+                if [ ! "${gql:-}" ]
                 then
                     gql.build _gql_doc "$op" data "$@"
                     gql=$( gql.format _gql_doc "$op" )
@@ -54,17 +57,18 @@ function gql
                 then
                     echo "$gql"
                 else
-                    echo "$gql" | 
-                        gql.agent "$GQL_URL" | 
+                    _gql_verbose "QUERY:\n$gql\n"
+                    echo "$gql" |
+                        gql.agent "$GQL_URL" |
                         gql.output-doc _gql_doc "$op"
                 fi
-            }                
+            }
             ;;
             
         schema)
-            gql.schema | jq . 
+            gql.schema | jq .
             ;;
-            
+
         *)
             _gql_usage_error "Op '$op' is not valid"
             ;;
@@ -79,7 +83,7 @@ function gql.query
     local -A _gql_doc
     gql.build _gql_doc "$op" data "$@"
     gql.format _gql_doc query | gql.agent "$GQL_URL"
-}    
+}
 
 ##############################################################################
 ##############################################################################
@@ -89,10 +93,10 @@ function gql.build
     local -n _doc="$1"; shift || _gql_required 'gql document variable reference'
     local docpath="$1"; shift || _gql_required 'gql document path'
     local datapath="$1"; shift || _gql_required 'gql result data path'
-    
+
     local -a _args=( "$@" )
     local -i _idx=0 _nargs="${#_args[@]}"
-    
+
     _gql_build "$docpath" "$datapath"
 }
 
@@ -103,11 +107,11 @@ function _gql_build
     local buildpath="${1:-$docpath}"; shift
     local builddata="${1:-$datapath}"; shift
     _doc["${docpath}:datapath"]="$datapath"
-    
+
     # These two references will be based on the value of $buildpath upon use.
     local -n paramstr='_doc[$buildpath:paramstr]'
     local -n fields='_doc[$buildpath:fields]'
-    
+
     _doc["${docpath}:node"]="${docpath##*.}"
 
     while [ $_idx -lt $_nargs ]
@@ -117,12 +121,12 @@ function _gql_build
         '[')
             _doc["${buildpath}:mode"]=table
             ;&
-            
+
         '{')
             _idx+=1
             _gql_build "$buildpath" "$builddata"
             ;;
-            
+
         '}' | ']')
             _idx+=1
             return
@@ -132,22 +136,21 @@ function _gql_build
             {
                 local node suffix name value="${arg#*=}"
                 _idx+=1
-                
+
                 if [ "$arg" != "$value" ]
                 then
                     name="${arg%%=*}"
                     value="${value//\"/\\\"}"
-                    value="${value//\n/\\n}"
+                    #value="${value//\n/\\n}"
                     value="\"$value\""
-                else                    
+                else
                     value="${arg#*:}"
                     if [ "$arg" != "$value" ]
                     then
                         name="${arg%%:*}"
-                        _gql_dump name value
                     fi
                 fi
-                
+
                 if [ "$name" ]
                 then
                     if [ "$value" ]
@@ -168,25 +171,25 @@ function _gql_build
                         else
                             {
                                 node="${arg%%.*}"
+                                arg="$suffix"
                                 if [ ! "${_doc["$buildpath.$node:fields"]}" ]
                                 then
                                     fields+="${fields:+ }$node"
-                                fi                                    
+                                fi
                                 buildpath+=".$node"
                                 builddata+=".$node"
                                 _doc["$buildpath:node"]="$node"
-                                arg="$suffix"
                             }
                         fi
                     done
-                    
+
                     fields+="${fields:+ }$arg"
                     _doc["$buildpath.$arg:node"]="$arg"
                     buildpath+=".$arg"
                     builddata+=".$arg"
                     _doc["${buildpath}:datapath"]="$builddata"
                 fi
-            }                
+            }
             ;;
         esac
     done
@@ -209,15 +212,15 @@ function _gql_format
 {
     local docpath="$1"; shift || _gql_required 'gql document path'
     local prefix="$1"; shift
-    
+
     echo -n "$prefix${_doc["$docpath":node]}"
-    
+
     local paramstr="${_doc["$docpath":paramstr]}"
     if [ "$paramstr" ]
     then
         echo -n "($paramstr)"
     fi
-    
+
     local field fields="${_doc["$docpath":fields]}"
     if [ "$fields" ]
     then
@@ -237,14 +240,18 @@ function _gql_format
 
 function gql.agent
 {
-    "${GQL_AGENT:-gql.agent.curl}" "$@" |
+    local -A _headers
+    
+    gql.sso.headers _headers
+    
+    "${GQL_AGENT:-gql.agent.curl}" -H _headers "$@" |
         jq '
             if has("errors")
             then
                 ( .errors[] | [ error(.message) ] )
             else
                 ( . )
-            end                
+            end
         '
 }
 
@@ -253,20 +260,30 @@ function gql.agent.curl
     local OPTIND OPTARG OPT
     local url="${GQL_URL}"
     local gql='-'
-    
-    while getopts 'u:' OPT
+    local -n headers
+    local -a curlopts
+
+    while getopts 'u:H:' OPT
     do
         case "$OPT" in
         u) url="$OPTARG";;
         d) gql="$OPTARG";;
+        H) headers="$OPTARG";;
+        O) curlopts+=( "$OPTARG" );;
         esac
     done
     shift $(( OPTIND - 1 ))
-    
+
     if [ "$gql" = - ]
     then
         gql="$(cat -)"
     fi
+    
+    local header
+    for header in "${!headers[@]}"
+    do
+        curlopts+=( -H "$header: ${headers[$header]}" )
+    done
     
     echo {} | jq --arg gql "$gql" '
     {
@@ -276,8 +293,56 @@ function gql.agent.curl
         -X POST \
         -H 'Content-Type: application/json' \
         -H 'Accept: application/json' \
+        "${curlopts[@]}" \
         --data-binary @- \
         "$url"
+}
+
+##############################################################################
+
+function gql.sso.headers
+{
+    local _sso_hdrs_var="$1"; shift || _gql_required 'headers dictionary'
+    
+    if [ "$GQL_SSO_METHOD" ]
+    then
+        gql.sso.${GQL_SSO}.headers "${_sso_hdrs_var}"
+    fi
+}
+
+function gql.sso.oauth.headers
+{
+    local -n _sso_hdrs="$1"; shift || _gql_required 'headers dictionary'
+    local bearer="${GQL_OAUTH_TOKEN}"
+    if [ ! "$bearer" ]
+    then
+        local url="${GQL_OAUTH_URL}"
+        local client_id="${GQL_OAUTH_CLIENT_ID}"
+        local secret="${GQL_OAUTH_SECRET}"
+        
+        if [ "$url" -a "$client_id" -a "$secret" ]
+        then
+            local token_type created_at expires_in 
+            read token_type bearer created_at expires_in  < <(
+                _gql_debug "Attempting to get bearer token from '$url' via '$client_id'"
+                curl -s \
+                    --request POST \
+                    -H 'Content-type: application/json' \
+                    -H 'Accept: application/json' \
+                    "$url?grant_type=client_credentials&client_id=$client_id&client_secret=$secret" |
+                jq -r '
+                [
+                    .token_type, 
+                    .access_token, 
+                    .created_at, 
+                    .expires_in
+                ]|@tsv' 
+            )
+            _gql_debug "Got bearer token $bearer"
+        fi
+    fi
+    
+    _sso_hdrs['Authorization']="Bearer $bearer"
 }
 
 ##############################################################################
@@ -287,13 +352,19 @@ function gql.output
 {
     if [ $# -gt 1 ]
     then
-        _gql_debug_cmd "gql.output.${GQL_OUTPUT:-jq}" "$@"
+        _gql_debug_cmd "gql.output.${GQL_OUTPUT:-jq}" "$@" |
+        if [ ! -p 1 ] && command -v column &> /dev/null
+        then
+            column -t -s '	'
+        else
+            cat -
+        fi
     elif [ -t 1 ]
     then
         jq .
     else
         cat -
-    fi        
+    fi
 }
 
 function gql.output-doc
@@ -301,9 +372,9 @@ function gql.output-doc
     local -n _doc="$1"; shift || _gql_required 'gql parsed document'
     local docpath="$1"; shift || _gql_required 'gql root path'
     local -a _outputs
-    
+
     _gql_outputs_from_doc "$docpath"
-    
+
     gql.output "${_outputs[@]}"
 }
 
@@ -311,16 +382,16 @@ function _gql_outputs_from_doc
 {
     local path="$1"; shift || _gql_required 'document path'
     local prefix="$1"
-    
+
     local mode="${_doc[$path:mode]}"
     local datapath="${_doc[$path:datapath]}"
-    
+
     if [ "$mode" = 'table' ]
     then
         _outputs+=( ".$datapath[" )
         prefix="$datapath."
     fi
-    
+
     local fields="${_doc[$path:fields]}"
     if [ "$fields" ]
     then
@@ -332,12 +403,12 @@ function _gql_outputs_from_doc
     else
         _outputs+=( ".${datapath#$prefix}" )
     fi
-    
+
     if [ "$mode" = 'table' ]
     then
         _outputs+=( ']' )
     fi
-    
+
 }
 
 ##############################################################################
@@ -347,14 +418,14 @@ function gql.output.jq
     local jqexpr
     _gql_make_jql "$@"
     _gql_dump jqexpr
-    
+
     jq -r "($jqexpr)"
 }
 
 function _gql_make_jql
 {
     local term comma
-    
+
     for term in "$@"
     do
         if [ "${term%[}" != "$term" ]
@@ -368,7 +439,7 @@ function _gql_make_jql
         else
             jqexpr+="$comma$term"
             comma=,
-        fi            
+        fi
     done
 }
 
@@ -411,7 +482,7 @@ function _gql_fatal
 function _gql_dump
 {
     _gql_debug_raw ""
-    _gql_debug_raw "At $(caller 0):" 
+    _gql_debug_raw "At $(caller 0):"
     local _var
     for _var in "$@"
     do
@@ -436,8 +507,8 @@ function _gql_internal_error
 
 function _gql_usage_error
 {
-    _fql_fatal 65 "$@"
-}    
+    _gql_fatal 65 "$@"
+}
 
 function _gql_required
 {
